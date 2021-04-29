77e890e17e9fb47cbd87d6f70df2450792f62b87
Aman Kumar <67067864+amankumar11@users.noreply.github.com>
2021-04-29 22:45:05 +0530

    Create RectanglePattern.cpp

---
+++ RectanglePattern.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int rows;
+    int c;
+    cin>>rows;
+    cin>>c;
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<c;j++){
+            cout<<"* ";
+            
+        }
+        cout<<endl;
+    }
+    return 0;
+}
